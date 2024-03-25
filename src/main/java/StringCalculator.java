cd4d38337ed13fcd7f160e00365342f333036329
chris.tmars <chris.tmars@kakaocorp.com>
2024-03-25 21:27:30 +0900

    feat: 하나의 입력 혹은 콜론과 콤마로 구분된 입력

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+
 public class StringCalculator {
 	public static int calculate(String input) {
+		int result = 0;
 		if (input == null || input.isEmpty()) {
 			return 0;
 		}
-		return 1;
+		String[] values = input.split(",|:");
+		for (String value : values) {
+			result += Integer.parseInt(value);
+		}
+		return result;
 	}
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,11 +1,25 @@
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringCalculatorTest {
 	@Test
 	void 빈문자열_또는_null() {
 		assertThat(StringCalculator.calculate(null)).isEqualTo(0);
 		assertThat(StringCalculator.calculate("")).isEqualTo(0);
+	}
+
+	@Test
+	void 쉼표_콜론을_구분자로_가지는_입력() {
+		assertThat(StringCalculator.calculate("1,2,3")).isEqualTo(6);
+		assertThat(StringCalculator.calculate("1,2:3")).isEqualTo(6);
+		assertThat(StringCalculator.calculate("1:2:3")).isEqualTo(6);
+	}
+
+	@Test
+	void 하나의_수_입력() {
+		assertThat(StringCalculator.calculate("1")).isEqualTo(1);
 	}
 }
